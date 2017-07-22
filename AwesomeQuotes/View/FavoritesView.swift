71dc9a0a670f43c54126bf61ae5cd51e085f2f13
Timo Jääskeläinen <timo.jaaskelainen@reaktor.com>
2017-07-22 11:46:02 -0400

    Add list of favorite quotes to the Favorites screen

--- AwesomeQuotes/View/FavoritesView.swift
+++ AwesomeQuotes/View/FavoritesView.swift
@@ -1,12 +1,16 @@
 import UIKit
 import Cartography
 
+let FavoritesCellIdentifier = "FavoritesCell"
+
 class FavoritesView: UIView {
 
     let favoritesLabel = UILabel()
     let closeButton = CustomButton("X")
-
+    let quotesTableView = UITableView()
     let closeHandler: () -> ()
+
+    var viewModel: FavoritesViewModel?
 
     init(closeHandler: @escaping () -> ()) {
         self.closeHandler = closeHandler
@@ -15,27 +19,43 @@ class FavoritesView: UIView {
 
         backgroundColor = UIColor.white
         translatesAutoresizingMaskIntoConstraints = true
+
         favoritesLabel.text = "Favorites"
+        quotesTableView.register(FavoritesCell.self, forCellReuseIdentifier: FavoritesCellIdentifier)
+        quotesTableView.dataSource = self
+        quotesTableView.delegate = self
+        quotesTableView.estimatedRowHeight = 40
+        quotesTableView.rowHeight = UITableViewAutomaticDimension
+        quotesTableView.separatorStyle = .none
+
 
         let subViews: [UIView] = [
             favoritesLabel,
             closeButton,
+            quotesTableView
         ]
         subViews.forEach { addSubview($0) }
 
         closeButton.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
 
-        constrain(self, favoritesLabel, closeButton) {
-            (containerView, favoritesLabel, closeButton) in
-            favoritesLabel.top == containerView.topMargin + 20
+        constrain(self, favoritesLabel, closeButton, quotesTableView) {
+            (containerView, favoritesLabel, closeButton, quotesTableView) in
+            favoritesLabel.top == containerView.top + 30
             favoritesLabel.centerX == containerView.centerX
 
-            closeButton.top == containerView.topMargin + 20
+            closeButton.centerY == favoritesLabel.centerY
             closeButton.trailing == containerView.trailingMargin
+
+            quotesTableView.top == favoritesLabel.bottom
+            quotesTableView.leading == containerView.leading
+            quotesTableView.trailing == containerView.trailing
+            quotesTableView.bottom == containerView.bottom
         }
     }
 
     func updateView(viewModel: FavoritesViewModel) {
+        self.viewModel = viewModel
+        self.quotesTableView.reloadData()
     }
 
     func closeButtonTapped() {
@@ -46,4 +66,21 @@ class FavoritesView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+}
+
+extension FavoritesView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.viewModel?.favoriteQuotes.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FavoritesCellIdentifier, for: indexPath) as? FavoritesCell else {
+            return UITableViewCell()
+        }
+        guard let quote = self.viewModel?.favoriteQuotes[indexPath.row] else {
+            return UITableViewCell()
+        }
+        cell.updateView(viewModel: FavoritesCellViewModel(quote: quote))
+        return cell
+    }
 }
--- AwesomeQuotes/View/FavoritesViewModel.swift
+++ AwesomeQuotes/View/FavoritesViewModel.swift
@@ -3,4 +3,8 @@ import Foundation
 struct FavoritesViewModel {
 
     let state: AppState
+
+    var favoriteQuotes: [Quote] {
+        return state.quotes.filter { $0.isFavorite }
+    }
 }
