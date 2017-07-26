4070403171ee81a636b756a2d73c8b39e6b1357c
Timo Jääskeläinen <timo.jaaskelainen@reaktor.com>
2017-07-26 08:22:13 -0400

    Remove unused state from the view model

--- AwesomeQuotes/View/FavoritesViewModel.swift
+++ AwesomeQuotes/View/FavoritesViewModel.swift
@@ -2,11 +2,9 @@ import Foundation
 
 struct FavoritesViewModel {
 
-    let state: AppState
     let quotesToDisplay: [Quote]
 
     init(state: AppState, initialQuoteIds: [Int]) {
-        self.state = state
         self.quotesToDisplay = state.quotes.reduce([]) { (result, quote) in
             return result + (initialQuoteIds.contains(quote.id) ? [quote] : [])
         }
