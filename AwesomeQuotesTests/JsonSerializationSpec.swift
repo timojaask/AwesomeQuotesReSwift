c729d6097f0b2d3c13f61ac081a489ebf521c909
Timo Jääskeläinen <timo.jaaskelainen@reaktor.com>
2017-07-29 13:06:18 -0400

    Add more test coverage to JsonSerialization

--- AwesomeQuotesTests/JsonSerializationSpec.swift
+++ AwesomeQuotesTests/JsonSerializationSpec.swift
@@ -20,18 +20,63 @@ class JsonSerializationSpec: QuickSpec {
 
             describe("fromJson") {
 
-                it("returns a valid quote object") {
-
-                    let input = JsonSerializationSpec.sampleQuotesJson.first!
-                    let expected = JsonSerializationSpec.sampleQuotes.first!
-                    let actual = try? Quote.fromJson(json: input)
+                it("returns a valid quote object when isFavorite is not provided") {
+                    let expected = Quote(id: 0, text: "testText1", author: "testAuthor1", isFavorite: false)
+                    let json: [String: Any] =  [
+                        "id": expected.id,
+                        "text": expected.text,
+                        "author": expected.author
+                    ]
+                    let actual = try? Quote.fromJson(json: json)
 
                     expect(actual).to(equal(expected))
                 }
 
-                it("throws an error on invalid json") {
+                it("returns a valid quote object when isFavorite is provided") {
+                    let expected = Quote(id: 0, text: "testText1", author: "testAuthor1", isFavorite: true)
+                    let json: [String: Any] =  [
+                        "id": expected.id,
+                        "text": expected.text,
+                        "author": expected.author,
+                        "isFavorite": expected.isFavorite
+                    ]
+                    let actual = try? Quote.fromJson(json: json)
 
-                    let input = ["text": "testText"]
+                    expect(actual).to(equal(expected))
+                }
+
+                it("throws an error on json dictionary being incorrect type") {
+                    let input = [["text": "testText"]]
+                    let expected = JsonSerializationError.JsonParsingError
+
+                    expect { try Quote.fromJson(json: input) }.to(throwError(expected))
+                }
+
+                it("throws an error on missing ID parameter") {
+                    let input: [String: Any] =  [
+                        "text": "testText1",
+                        "author": "testAuthor1"
+                    ]
+                    let expected = JsonSerializationError.JsonParsingError
+
+                    expect { try Quote.fromJson(json: input) }.to(throwError(expected))
+                }
+
+                it("throws an error on missing text parameter") {
+                    let input: [String: Any] =  [
+                        "id": 0,
+                        "author": "testAuthor1"
+                    ]
+                    let expected = JsonSerializationError.JsonParsingError
+
+                    expect { try Quote.fromJson(json: input) }.to(throwError(expected))
+                }
+
+                it("throws an error on missing author parameter") {
+                    let input: [String: Any] =  [
+                        "id": 0,
+                        "text": "testText1"
+                    ]
                     let expected = JsonSerializationError.JsonParsingError
 
                     expect { try Quote.fromJson(json: input) }.to(throwError(expected))
@@ -50,6 +95,17 @@ class JsonSerializationSpec: QuickSpec {
                     let actual = try? jsonToQuotes(input)
 
                     expect(actual).to(equal(expected))
+                }
+
+                it("throws an error when json is not an array") {
+                    let input: [String: Any] =  [
+                        "id": 0,
+                        "text": "testText1",
+                        "author": "testAuthor1"
+                    ]
+                    let expected = JsonSerializationError.JsonParsingError
+
+                    expect { try jsonToQuotes(input) }.to(throwError(expected))
                 }
             }
         }
